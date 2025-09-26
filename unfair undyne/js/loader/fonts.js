c9622cee24e4425d134dfdf937a690fdfa4be71b
bubbls <132022772+bubbls@users.noreply.github.com>
2025-09-26 15:09:26 -0600

    bro

--- unfair undyne/js/loader/fonts.js
+++ unfair undyne/js/loader/fonts.js
@@ -3,9 +3,9 @@
 var fonts_loader = new PIXI.loaders.Loader();
 
 fonts_loader
-  .add("undertale", "unfair%20undyne/fonts/undertale.fnt")
-  .add("numbers", "unfair%20undyne/fonts/numbers.fnt")
-  .add("speechbubble", "unfair%20undyne/fonts/speechbubble.fnt")
+  .add("undertale", "../fonts/undertale.fnt")
+  .add("numbers", "../fonts/numbers.fnt")
+  .add("speechbubble", "../fonts/speechbubble.fnt")
   .on("complete", function(loader, resources) {
     process_fonts(resources);
   });
